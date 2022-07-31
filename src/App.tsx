7da8cd1c37a12df6f93c3555641003680f6b2366
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2022-07-31 09:01:20 +0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import Header from "./components/home/header";
 import { themeContext } from "./components/context/themecontext";
 import { useEffect, useState } from "react";
-// comment
-// comment updated
+
 import Main from "./components/main";
 const App = () => {
   const [theme, setTheme] = useState<string>("light");
